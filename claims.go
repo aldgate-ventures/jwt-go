63d42f22ee6aa518a1d89a2b8b693bac225b19a1
Adrià Cidre <adria.cidre@gmail.com>
2020-01-16 12:26:32 +0100

    fix problems  "Token used before issued"

--- claims.go
+++ claims.go
@@ -105,14 +105,14 @@ func verifyExp(exp int64, now int64, required bool) bool {
 	if exp == 0 {
 		return !required
 	}
-	return now <= exp
+	return now < exp
 }
 
 func verifyIat(iat int64, now int64, required bool) bool {
 	if iat == 0 {
 		return !required
 	}
-	return now >= iat
+	return now > iat
 }
 
 func verifyIss(iss string, cmp string, required bool) bool {
